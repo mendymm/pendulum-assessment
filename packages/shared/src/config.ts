bd4ce48d485d2934b3010d692caa9145453c9e5e
Mendy Man <git@mendy.dev>
2026-08-19 15:11:08 -0400

    update the config

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -1,7 +1,14 @@
 interface RuntimeConfig {
   gatewayPort: number;
+
+  // A sim will listen on a port computed from simStartPort + <my node id>
+  simStartPort:number;
+  // Number of pendulum sims
+  simCount:number;
 }
 
 export const RUNTIME_CONFIG: RuntimeConfig = {
-  gatewayPort: 9000,
+  gatewayPort: 8000,
+  simStartPort:9000,
+  simCount:5,
 };
