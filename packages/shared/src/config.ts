bdc06249f2cedd5b80f89736f6c3760cdfa806c7
Mendy Man <git@mendy.dev>
2026-08-20 10:34:22 -0400

    add simHz

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -5,10 +5,13 @@ interface RuntimeConfig {
   simStartPort: number;
   // Number of pendulum sims
   simCount: number;
+  // Number of simulation steps per second
+  simHz: number;
 }
 
 export const RUNTIME_CONFIG: RuntimeConfig = {
   gatewayPort: 8000,
   simStartPort: 9000,
   simCount: 5,
+  simHz: 120,
 };
