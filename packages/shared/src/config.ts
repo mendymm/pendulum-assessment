8b9dec411985887653991494d76da87efe63e0cb
Mendy Man <git@mendy.dev>
2026-08-21 10:39:51 -0400

    add ui update hz

--- packages/shared/src/config.ts
+++ packages/shared/src/config.ts
@@ -3,15 +3,21 @@ interface RuntimeConfig {
 
   // A sim will listen on a port computed from simStartPort + <my node id>
   simStartPort: number;
+
   // Number of pendulum sims
   simCount: number;
+
   // Number of simulation steps per second
   simHz: number;
+
+  // how often should the gateway push position updates to the UI
+  uiUpdateHz: number;
 }
 
 export const RUNTIME_CONFIG: RuntimeConfig = {
   gatewayPort: 8000,
   simStartPort: 9000,
-  simCount: 5,
+  simCount: 2,
   simHz: 120,
+  uiUpdateHz: 60,
 };
